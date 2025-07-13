88b7edde196d8e107b2e7d9c38d52b940535f12a
Udeesha-Prabhashana <udeeshaprabhashana123@gmail.com>
2025-07-14 02:15:03 +0530

    refactor: clean up imports and ensure consistent formatting in App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from "./components/layout/Header"
+import Footer from "./components/layout/Footer"
+import Home from "./pages/Home"
+import Services from "./pages/Services"
+import Blog from "./pages/Blog"
+import Contact from "./pages/Contact"
+import "./App.css"
+
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 // Detect if we're on GitHub Pages
@@ -22,3 +31,5 @@ function App() {
     </Router>
   );
 }
+
+export default App
