53b25b006d3813740e183f751ede70db99a7a996
Udeesha-Prabhashana <udeeshaprabhashana123@gmail.com>
2025-07-14 02:12:06 +0530

    refactor: switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Header from "./components/layout/Header"
-import Footer from "./components/layout/Footer"
-import Home from "./pages/Home"
-import Services from "./pages/Services"
-import Blog from "./pages/Blog"
-import Contact from "./pages/Contact"
-import "./App.css"
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+// Detect if we're on GitHub Pages
+const isGitHubPages = window.location.host.includes('github.io');
+const basename = isGitHubPages ? '/VALUER-PORTFOLIO' : '/';
 
 function App() {
   return (
-    <Router>
+    <Router basename={basename}>
       <div className="App">
         <Header />
         <main>
@@ -23,7 +20,5 @@ function App() {
         <Footer />
       </div>
     </Router>
-  )
+  );
 }
-
-export default App
